240d8f84c07bd33ea570fa58df21eb0fb254d1a3
SirTonsam <samutongai@gmail.com>
2023-04-27 14:56:03 -0700

    main.c

--- main.c
+++ main.c
@@ -1,32 +1,126 @@
 #include "shell.h"
 
 /**
- * main - Entry point of the shell
- * Return: 0 on (success)
+ * main - function initializes the variables of the program
+ * @argc: number of values received from the command line
+ * @argv: values received from the command line
+ * @env: number of values received from the command line
+ * Return: zero on succes.
  */
 
-int main(void)
+int main(int argc, char *argv[], char *env[])
 {
-	char *input, *cmd;/*pointer to character arrays*/
-	list_t *env_list;/*pointer to a linked list*/
-	int status;/*integer to store the status of the command line loop*/
+data_of_program data_struct = {NULL}, *data = &data_struct;
+char *prompt = "";
 
-	/*allocate memory for the input buffer using malloc ()*/
-	input = malloc(sizeof(char) * BUFF_SIZE);
+_init_data(data, argc, argv, env);
 
-	/*check if memory was allocated successfully*/
-	if (input == NULL)
-		return (1);/*return an error code if memory allocation failed*/
-	cmd = NULL;/*set the pointer to NULL*/
-	/*convert the environment variables array into a linked list*/
-	env_list = array_to_list(environ);
-	/*call the command line loop function and store the return status*/
-	status = cmd_line_loop(input, cmd, &env_list);
+signal(SIGINT, _handle_ctrl_c);
 
-	/*free the memory allocated for the linked list and input buffer*/
-	free_list(env_list);
-	free(input);
-	/*return the status of the command line loop to the calling function*/
-	return (status);
+if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
+{/* We are in the terminal, interactive mode */
+errno = 2;/*???????*/
+prompt = PROMPT_MSG;
+}
+errno = 0;
+_sisifo(prompt, data);
+return (0);
 }
 
+/**
+ * _handle_ctrl_c - print the prompt in a new line
+ * when the signal SIGINT (ctrl + c) is send to the program
+ * @UNUSED: option of the prototype
+ */
+
+void _handle_ctrl_c(int opr UNUSED)
+{
+_print("\n");
+_print(PROMPT_MSG);
+}
+
+/**
+ * _init_data - inicialize the struct with the info of the program
+ * @data: pointer to the structure of data
+ * @argv: array of arguments pased to the program execution
+ * @env: environ pased to the program execution
+ * @argc: number of values received from the command line
+ */
+
+void _init_data(data_of_program *data, int argc, char *argv[], char **env)
+{
+int i = 0;
+
+data->program_name = argv[0];
+data->input_line = NULL;
+data->command_name = NULL;
+data->exec_counter = 0;
+/* define the file descriptor to be readed*/
+if (argc == 1)
+data->file_descriptor = STDIN_FILENO;
+else
+{
+data->file_descriptor = open(argv[1], O_RDONLY);
+if (data->file_descriptor == -1)
+{
+_printe(data->program_name);
+_printe(": 0: Can't open ");
+_printe(argv[1]);
+_printe("\n");
+exit(127);
+}
+}
+data->tokens = NULL;
+data->env = malloc(sizeof(char *) * 50);
+if (env)
+{
+for (; env[i]; i++)
+{
+data->env[i] = _str_duplicate(env[i]);
+}
+}
+data->env[i] = NULL;
+env = data->env;
+
+data->alias_list = malloc(sizeof(char *) * 20);
+for (i = 0; i < 20; i++)
+{
+data->alias_list[i] = NULL;
+}
+}
+
+/**
+ * _sisifo - its a infinite loop that shows the prompt
+ * @prompt: prompt to be printed
+ * @data: its a infinite loop that shows the prompt
+ */
+
+void _sisifo(char *prompt, data_of_program *data)
+{
+int error_code = 0, string_len = 0;
+
+while (++(data->exec_counter))
+{
+_print(prompt);
+error_code = string_len = _get_line(data);
+
+if (error_code == EOF)
+{
+_free_all_data(data);
+exit(errno); /* if EOF is the fisrt Char of string, exit*/
+}
+if (string_len >= 1)
+{
+_expand_alias(data);
+_expand_variables(data);
+_tokenize(data);
+if (data->tokens[0])
+{ /* if a text is given to prompt, _execute */
+error_code = _execute(data);
+if (error_code != 0)
+_print_error(error_code, data);
+}
+_free_recurrent_data(data);
+}
+}
+}
