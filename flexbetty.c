95cbcd6ef6bce81004b8942a58ec3e473af0796a
SirTonsam <samutongai@gmail.com>
2023-04-27 17:00:15 -0700

    pass brtty

---
+++ flexbetty.c
@@ -0,0 +1,16 @@
+include "shell.h"
+
+/**
+ * print_sum - Prints the sum of two integers
+ *
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * This function prints the sum of two integers to standard output.
+ */
+void print_sum(int a, int b)
+{
+    int sum = a + b;
+
+    printf("The sum of %d and %d is %d\n", a, b, sum);
+}
